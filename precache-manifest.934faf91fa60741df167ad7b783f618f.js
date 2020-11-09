6e292590fa231d62cbc9a84e302381e6815e6d99
Bright Kanyange <brightkanyange@outlook.com>
2020-11-09 15:13:52 +0300

    Updates

---
+++ precache-manifest.934faf91fa60741df167ad7b783f618f.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "85895f3c56b1585b8f5a",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/0.96574bf7.chunk.js"
+  },
+  {
+    "revision": "872e6f9c15cb10d0d758",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/css/main.7eaebf4d.chunk.css"
+  },
+  {
+    "revision": "872e6f9c15cb10d0d758",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/main.ea002db7.chunk.js"
+  },
+  {
+    "revision": "708fa515081adfb8965e",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/runtime~main.2456c020.js"
+  },
+  {
+    "revision": "355fb55c09f5fc356d87",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/3.c7803549.chunk.js"
+  },
+  {
+    "revision": "844f96ab850239bcfab4",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/4.f8cb6488.chunk.js"
+  },
+  {
+    "revision": "8b7537dc7aa59ce53014",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/5.7bcb8dc1.chunk.js"
+  },
+  {
+    "revision": "91f4503e015e42913fc7",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/6.0a7ccc5a.chunk.js"
+  },
+  {
+    "revision": "6ab890ce1a69a8d1d344",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/7.e21a0379.chunk.js"
+  },
+  {
+    "revision": "b29f6bab3bb0fda50f06",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/8.99ea1170.chunk.js"
+  },
+  {
+    "revision": "d3ac5fb2f417e7b62180",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/9.86976053.chunk.js"
+  },
+  {
+    "revision": "27d9f07c84c41a6f324f",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/10.28c27ed1.chunk.js"
+  },
+  {
+    "revision": "16db9ddb772fe768a24d",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/11.cff8022b.chunk.js"
+  },
+  {
+    "revision": "4a74bbbb0ceaa4a0948e",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/12.fe02c7dd.chunk.js"
+  },
+  {
+    "revision": "485b6ab5e99b40176069",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/13.a6e19803.chunk.js"
+  },
+  {
+    "revision": "7d4b0724344f0166abac",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/14.e281063c.chunk.js"
+  },
+  {
+    "revision": "b010b505fb74aaec451d",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/15.6cab0df1.chunk.js"
+  },
+  {
+    "revision": "cbeead664bc9b447f185",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/16.353392ef.chunk.js"
+  },
+  {
+    "revision": "8aad327d3940a24254dc",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/17.e172572b.chunk.js"
+  },
+  {
+    "revision": "ba91f5d980292866e27b",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/18.922b67af.chunk.js"
+  },
+  {
+    "revision": "f8eefd8abb6d47ec4e43",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/js/19.17cdba3a.chunk.js"
+  },
+  {
+    "revision": "734d93d326019c132cb32ef8a15e95c6",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/logo.734d93d3.png"
+  },
+  {
+    "revision": "c9d353a0aff16c06991a694ad3e04b24",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/sidebar-8.c9d353a0.jpg"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "e061fda3096d541152191d26df8dfefb",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/solitonbg.e061fda3.png"
+  },
+  {
+    "revision": "b7f68e8afaec5bfad8fdcc44e4e4e483",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/background_1920-11.b7f68e8a.jpg"
+  },
+  {
+    "revision": "acfe7046f47d96b9cb598622e50fe610",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/background_1920-18.acfe7046.jpg"
+  },
+  {
+    "revision": "1420d15b22ed3f39abc468212d735814",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/background_640-1.1420d15b.jpg"
+  },
+  {
+    "revision": "55e47e98ef3c84cf5eb4706a4c74311a",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/background_640-3.55e47e98.jpg"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "1d98ff2e44d70e391ec7a49e4efdfa86",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/background_1920-5.1d98ff2e.jpg"
+  },
+  {
+    "revision": "1e0eb5503400ca7f2a65175cec9e9115",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/background_1920-6.1e0eb550.jpg"
+  },
+  {
+    "revision": "abf47fc19906e17185a37765e7594ee0",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/background_1920-7.abf47fc1.png"
+  },
+  {
+    "revision": "22fc19f2c02f7dba20864f097f0bfd20",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/static/media/background_1920-19.22fc19f2.jpg"
+  },
+  {
+    "revision": "a7d3100c0b669e5e2ca7b915c87b92a7",
+    "url": "https://brightkan.github.io/soliton-bakozi-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://reduction-admin.github.io/react-reduction/precache-manifest.fb8d461cb20289cbf7ba53d6fb98f7b2.js"
+  "https://brightkan.github.io/soliton-bakozi-frontend/precache-manifest.934faf91fa60741df167ad7b783f618f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://reduction-admin.github.io/react-reduction/index.html", {
+workbox.routing.registerNavigationRoute("https://brightkan.github.io/soliton-bakozi-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
